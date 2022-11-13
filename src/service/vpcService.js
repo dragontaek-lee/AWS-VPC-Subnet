2ffa372ce57cdb25e8a8451e0c257bfbfa828ce2
unknown <dragontaek98@naver.com>
2022-11-13 15:25:14 +0900

    feat: aws-sdk connection test

--- src/service/vpcService.js
+++ src/service/vpcService.js
@@ -1,4 +1,12 @@
+import AWS from "aws-sdk";
+
 const getVPCInfo = async () => {
+    AWS.config.getCredentials(function(err) {
+        if (err) console.log(err.stack);
+        else {
+          console.log("Access key:", AWS.config.credentials.accessKeyId);
+        }
+      });
     return "VPCInfo";
 };
 
